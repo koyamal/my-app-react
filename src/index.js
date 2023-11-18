1a9e1494beab09c228a66b7ec573bf16d2e06928
koyamal <tanakamaruko1121n@gmail.com>
2023-11-18 21:44:07 +0900

    Edit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function Square(props) {
+  console.log(props);
   return (
     <button className="square" onClick={() => props.onClick()}>
       {props.value}
