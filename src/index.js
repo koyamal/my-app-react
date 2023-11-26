c77df05b6d4cd45f3cceb9cfcb5e779fe693e009
koyamal <tanakamaruko1121n@gmail.com>
2023-11-26 21:13:58 +0900

    削除した

--- src/index.js
+++ src/index.js
@@ -80,9 +80,6 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    console.log(this.state.stepNumber);
-    console.log("render history", history);
-    console.log("render current", current);
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
