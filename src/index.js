191ff04b1e7f6901445034ec56d98b34e839fae2
koyamal <tanakamaruko1121n@gmail.com>
2023-11-26 18:53:56 +0900

    エラーが出るのでデバッグ

--- src/index.js
+++ src/index.js
@@ -59,6 +59,7 @@ class Game extends React.Component {
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
+    console.log("handleClick current", current);
     const squares = current.squares.slice();
     if(squares[i] || calculateWinner(squares)) return;
     squares[i] = this.state.xIsNext? 'X': 'O';
